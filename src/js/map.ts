673fe2975122e46761ed5ad5fefef298942f0cdf
Patrik Andó <andopatrik0@gmail.com>
2018-11-15 22:24:47 +0100

    [ADD] Map broken version

--- src/js/map.ts
+++ src/js/map.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import * as L from '../../node_modules/leaflet'
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoicGF0cmlrYW5kbyIsImEiOiJjam9pOWpoYTcwMGVtM3BzMXM4ZTd2b20wIn0.442-FW0yJ6x0M2txuKy1Xw'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
