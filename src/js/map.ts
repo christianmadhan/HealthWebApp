773f594e0355eda57cd3f7e3b687b4c201711e57
KongChristian <christianmadhan@gmail.com>
2018-11-16 21:52:22 +0100

    [CLEAN-UP] Map
    Moved script files from map.html to map.ts and it works!

--- src/js/map.ts
+++ src/js/map.ts
@@ -1,9 +1,50 @@
 import * as $  from "../../node_modules/jquery/dist/jquery";
+import * as L from "../../node_modules/leaflet/dist/leaflet-src";
 let profilePic: HTMLImageElement = <HTMLImageElement>document.getElementById("ProfileAvatar");
+let leafMap: HTMLDivElement = <HTMLDivElement>document.getElementById("mapid");
 
 $(document).ready(function() {
     let getStoredUserID = localStorage.getItem("key");
     let LoggedInUserID = parseInt(getStoredUserID);
      profilePic.src = "assets/img/avatar" + LoggedInUserID + ".jpg";
    });
+
+
+
+   var mymap = L.map(leafMap).setView([51.505, -0.09], 13);
+    
+   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+     maxZoom: 18,
+     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+     id: 'mapbox.streets'
+   }).addTo(mymap);
+ 
+   L.marker([51.5, -0.09]).addTo(mymap)
+     .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+ 
+   L.circle([51.508, -0.11], 500, {
+     color: 'red',
+     fillColor: '#f03',
+     fillOpacity: 0.5
+   }).addTo(mymap).bindPopup("I am a circle.");
+ 
+   L.polygon([
+     [51.509, -0.08],
+     [51.503, -0.06],
+     [51.51, -0.047]
+   ]).addTo(mymap).bindPopup("I am a polygon.");
+ 
+ 
+   var popup = L.popup();
+ 
+   function onMapClick(e:any) {
+     popup
+       .setLatLng(e.latlng)
+       .setContent("You clicked the map at " + e.latlng.toString())
+       .openOn(mymap);
+   }
+ 
+   mymap.on('click', onMapClick);
   
