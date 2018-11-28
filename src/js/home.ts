d8bb14061cf9da80cf16034aeb7d5660f5ab3ca6
Daniel <dani337x@edu.easj.dk>
2018-11-28 09:45:47 +0100

    Home page update

--- src/js/home.ts
+++ src/js/home.ts
@@ -3,6 +3,7 @@ import axios, {
     AxiosError
 } from "../../node_modules/axios/index";
 import * as $  from "../../node_modules/jquery/dist/jquery";
+import * as rssparser from "../../node_modules/rss-parser/dist/rss-parser";
 
 
 let loggedInUserId = parseInt(localStorage.getItem("key"));
@@ -18,10 +19,11 @@ interface IUser {
 $(document).ready(function() {
     let getStoredUserID = localStorage.getItem("key");
     let loggedInUserId = parseInt(getStoredUserID);
-    loadUserData();
+    loadUserData();    
 });
 
- function loadUserData() : void {
+
+function loadUserData() : void {
      
     try {
 
@@ -47,4 +49,70 @@ $(document).ready(function() {
     let getStoredUserID = localStorage.getItem("key");
     let LoggedInUserID = parseInt(getStoredUserID);
      profilePic.src = "assets/img/avatar" + LoggedInUserID + ".jpg";
+
+    getBMIData();
+
+    getRSS();
 });
+
+
+
+
+interface IBMIData {
+    weight: number;
+    height: number;
+    userID: number;
+}
+
+let localuserid: number = parseInt(localStorage.getItem("key"));
+
+function getBMIData(): void {
+    let uri: string = "https://berthaprojectusersapi.azurewebsites.net/api/HealthDatas/";
+
+    axios.get<IBMIData[]>(uri)
+        .then(function (response: AxiosResponse<IBMIData[]>): void {
+            console.log(response.data);
+            let resultweight: number;
+            let resultheight: number;
+
+            response.data.forEach((hd: IBMIData) => {
+                if (hd.userID === localuserid){
+                    resultweight = hd.weight;
+                    resultheight = hd.height;
+                }
+            });
+            console.log(resultweight + " | " + resultheight);
+
+            let bminmbr: number = (Math.round(((resultweight / ((resultheight/100) * (resultheight/100))))));
+            let bmicolour: string = "";
+
+            /* Under 18.5 – you are considered underweight and possibly malnourished.
+            18.5 to 24.9 – you are within a healthy weight range for young and middle-aged adults.
+            25.0 to 29.9 – you are considered overweight.
+            Over 30 – you are considered obese.*/
+            if (bminmbr < 19) {bmicolour="#9beeff"} else if (bminmbr >= 19 && bminmbr < 25) {bmicolour="#3fff62"} else if (bminmbr >= 25) {bmicolour="#ff0a0a"}
+            
+            let bmiinfo: HTMLDivElement = <HTMLDivElement>document.getElementById("bmibox");
+            bmiinfo.innerHTML = "BMI: <font color=&quot;${bmicolour}&quot;>" + bminmbr + "</font>";
+        })
+        .catch(function (error: AxiosError): void {
+            console.log(error);
+        })
+}
+
+
+
+
+
+
+
+
+/* RSS */
+function getRSS(): void{
+    let feed: HTMLDivElement = <HTMLDivElement>document.getElementById("rssfeed");
+
+    var req = new XMLHttpRequest();  
+    req.open('GET', 'https://www.sciencedaily.com/rss/top/environment.xml', false);   
+    req.send(null);  
+    if(req.status == 200) {feed.innerHTML=req.toString();}
+}
